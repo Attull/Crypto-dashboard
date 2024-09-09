14aa31ee837d9c118f5d8b8310a0a5eeaf22b77d
Attull <atul852798@gmail.com>
2024-09-09 21:35:13 +0530

    add eur option in currecny drop down

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,6 +17,7 @@ const Navbar = () => {
                 <option value="usd">USD</option>
                 <option value="eur">EUR</option>
                 <option value="inr">INR</option>
+                <option value="eur">EUR</option>
             </select>
             <button>Sign up</button>
         </div>
